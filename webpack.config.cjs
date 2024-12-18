3d377617dfe157d21260ddf43abcccb59eb3612f
artm512 <44886239+artm512@users.noreply.github.com>
2024-12-18 15:07:26 +0900

    webpackとbabelの設定

---
+++ webpack.config.cjs
@@ -0,0 +1,39 @@
+/** @type {import('webpack').Configuration} */
+
+const webpack = require("webpack");
+const nodeExternals = require("webpack-node-externals");
+const path = require("path");
+
+const BUILD_ROOT = path.join(__dirname, "./dist");
+const SRC_ROOT = path.join(__dirname, "./src");
+
+module.exports = {
+  context: SRC_ROOT,
+  entry: path.resolve("server", "index.ts"),
+  mode: "development",
+  externals: [nodeExternals()],
+  output: {
+    filename: "server.cjs",
+    path: BUILD_ROOT,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        loader: "ts-loader",
+        options: {
+          configFile: "tsconfig.json",
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js", ".json"],
+  },
+};
